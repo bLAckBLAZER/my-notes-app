4503d16071ed56b75df0da987f5a6ab2ec71042e
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2022-05-28 17:18:00 +0530

    added new test credentials

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -8,10 +8,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Omkar",
+    lastName: "Jadhav",
+    email: "omj@gmail.com",
+    password: "test123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
